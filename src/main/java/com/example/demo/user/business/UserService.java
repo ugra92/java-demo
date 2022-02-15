43141287aa36f87f8a90e5fad25504c97ce54b16
Ugra <ugra1992@gmail.com>
2022-02-15 16:41:26 +0100

    fix database connection.

--- src/main/java/com/example/demo/user/business/UserService.java
+++ src/main/java/com/example/demo/user/business/UserService.java
@@ -1,6 +1,8 @@
 package com.example.demo.user.business;
 
+import com.example.demo.user.database.UserRepository;
 import com.example.demo.user.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,8 +10,15 @@ import java.util.List;
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public List<User> getUsers()
     {
-        return List.of(new User());
+        return userRepository.findAll();
     }
 }
